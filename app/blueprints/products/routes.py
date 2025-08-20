139fcb772cb32e7722dde2eca2e6725ba2dcc63d
dotyapok <den.gokhveys@gmail.com>
2025-08-20 23:57:17 +0700

    gallery

--- app/blueprints/products/routes.py
+++ app/blueprints/products/routes.py
@@ -14,3 +14,10 @@ def product(product_id):
     product = Product.query.get_or_404(product_id)
     product.image_url = json.loads(product.image_url)
     return render_template('main/product_cart.html', product=product)
+
+@products_bp.route('/gallery')
+def gallery():
+    all_products = Product.query.order_by(Product.id.desc()).all()
+    for product in all_products:
+        product.image_url = json.loads(product.image_url)
+    return render_template('main/gallery.html', products=all_products)
