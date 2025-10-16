b6e55545f980fcac3d8f7753df2ae4b7eac117fc
amplisuda <den.gokhveys@mail.ru>
2025-10-16 23:23:36 +0700

    refactor

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from app.config import Config
 from app.models import db
 from app.blueprints.main.routes import main_bp
 from app.blueprints.products.routes import products_bp
-from app.blueprints.categories.routes import categories_bp
 
 def create_app():
     app = Flask(__name__)
@@ -14,7 +13,6 @@ def create_app():
     # /app/blueprints/
     app.register_blueprint(main_bp)
     app.register_blueprint(products_bp, url_prefix='/product')
-    app.register_blueprint(categories_bp, url_prefix='/categories')
 
     with app.app_context():
         db.create_all()
--- app/blueprints/categories/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-#from ...models import Image
-
-categories_bp = Blueprint('categories', __name__, template_folder='templates')
-
-@categories_bp.route('/<category>')
-def category(category):
-    #images = Image.query.filter_by(category=category).all()
-    return render_template('categories/category.html', images=images, category=category)
